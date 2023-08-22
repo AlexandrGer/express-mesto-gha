d47f0c2a3ae40cfc6e03c37535150d808fd1050d
Alexandr Gerasimov <sashagerasimov55@gmail.com>
2023-08-22 16:57:28 +0300

    Добавил неизвестную страницу

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const usersRoutes = require('./users');
 const cardsRoutes = require('./cards');
+const notFoundRoutes = require('./notFoundPage');
 
 router.use('/users', usersRoutes);
 router.use('/cards', cardsRoutes);
+router.use('/*', notFoundRoutes);
 
 module.exports = router;
