deed56690f2973c0b88348ae2f15b28ee6b8f1cc
Alexandr Gerasimov <sashagerasimov55@gmail.com>
2023-08-30 20:28:13 +0300

    Финальные изменения v2

--- utils/constants.js
+++ utils/constants.js
@@ -1,8 +1,8 @@
 const SECRET_KEY = 'some-secret-key';
 
-const URL_REG = /^(http|https):\/\/(www\.)?[a-zA-Z0-9\--._~:/?#[\]@!$&'()*+,;=]+#?$/;
+// const URL_REG = /^(http|https):\/\/(www\.)?[a-zA-Z0-9\--._~:/?#[\]@!$&'()*+,;=]+#?$/;
 
 module.exports = {
   SECRET_KEY,
-  URL_REG,
+  // URL_REG,
 };
